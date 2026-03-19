e84125b0f30f06a9ac600e645166d4fc8d9cbcc6
wang2186223 <jannatjahan36487@gmail.com>
2026-03-19 10:04:52 +0800

    2026年3月19日   提交新广告js-2

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "5gvci.com",
-    "zoneId": 10746222
+    "zoneId": 10748784
 }
 self.lary = ""
 importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
